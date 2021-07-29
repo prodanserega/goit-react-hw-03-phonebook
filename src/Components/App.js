abaa98d8eb44145fefec4c2ee812076fbc8666a8
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-07-29 11:25:02 +0300

    localStorage

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,13 +9,25 @@ import "../index.css";
 export default class App extends Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     filter: "",
   };
+
+  componentDidMount() {
+    const localContacts = JSON.parse(localStorage.getItem("contacts"));
+
+    localContacts && this.setState({ contacts: localContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   handelAddContact = (newContact) =>
     this.setState(({ contacts }) => ({
